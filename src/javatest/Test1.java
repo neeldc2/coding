02a46dbeceda76181bfba970ba0da45dce714da6
neeld <neeldc2@gmail.com>
2024-08-10 21:08:28 +0530

    Testing Java

---
+++ src/javatest/Test1.java
@@ -0,0 +1,15 @@
+package javatest;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+
+        System.out.println(++a);  // Outputs 6
+        System.out.println(a);    // Outputs 6
+
+        System.out.println(b++);  // Outputs 5
+        System.out.println(b);    // Outputs 6
+    }
+}
